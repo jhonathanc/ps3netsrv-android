dd4fdc5094f1127ae19addea3278541f085f61e7
Jhonathan Corrêa <jhonathan.correa@outlook.com>
2023-03-04 00:26:07 -0300

    fix: remove imports

--- app/src/main/java/com/jhonju/ps3netsrv/utils/Utils.java
+++ app/src/main/java/com/jhonju/ps3netsrv/utils/Utils.java
@@ -1,11 +1,8 @@
 package com.jhonju.ps3netsrv.utils;
 
-import static android.content.Context.WIFI_SERVICE;
-
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 
 import com.jhonju.ps3netsrv.PS3NetSrvApp;
 
@@ -14,30 +11,19 @@ import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.Enumeration;
 
 public class Utils {
 
-    /**
-     * Get IP address from first non-localhost interface
-     *
-     * @param useIPv4 true=return ipv4, false=return ipv6
-     * @return address or empty string
-     */
     public static String getIPAddress(boolean useIPv4) throws Exception {
         ConnectivityManager connManager = (ConnectivityManager) PS3NetSrvApp.getAppContext().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = connManager.getActiveNetworkInfo();
 
         if (activeNetwork != null && activeNetwork.isConnected()) {
-            // Obtém a lista de interfaces de rede disponíveis
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
 
             while (networkInterfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = networkInterfaces.nextElement();
-
-                // Verifica se a interface de rede está conectada e é a mesma que a conexão ativa
                 if (networkInterface.isUp() && !networkInterface.isLoopback() && networkInterface.getInterfaceAddresses().size() > 0) {
                     for (InterfaceAddress address : networkInterface.getInterfaceAddresses()) {
                         InetAddress inetAddress = address.getAddress();

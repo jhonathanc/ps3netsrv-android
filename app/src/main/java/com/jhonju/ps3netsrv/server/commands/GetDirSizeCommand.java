0016873fb52d9628c95cc9903b26fa768516ae73
Jhonathan Correa <jhonathan.correa@outlook.com>
2023-03-20 15:00:34 -0300

    fix: GetDirSizeCommand: return a long instead int
    GetDirSizeCommand: return a long instead a int when an error occurs, so we must override getFile and send methods

--- app/src/main/java/com/jhonju/ps3netsrv/server/commands/GetDirSizeCommand.java
+++ app/src/main/java/com/jhonju/ps3netsrv/server/commands/GetDirSizeCommand.java
@@ -6,11 +6,38 @@ import com.jhonju.ps3netsrv.server.utils.Utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class GetDirSizeCommand extends FileCommand {
 
     public GetDirSizeCommand(Context ctx, short filePathLength) {
         super(ctx, filePathLength);
+    }
+
+    @Override
+    protected File getFile() throws IOException, PS3NetSrvException {
+        ByteBuffer buffer = Utils.readCommandData(ctx.getInputStream(), this.filePathLength);
+        if (buffer == null) {
+            send(Utils.longToBytesBE(ERROR_CODE));
+            throw new PS3NetSrvException("ERROR: command failed receiving filename.");
+        }
+        return new File(ctx.getRootDirectory(), new String(buffer.array(), StandardCharsets.UTF_8).replaceAll("\\x00+$", ""));
+    }
+
+    @Override
+    protected void send(byte[] result) throws IOException, PS3NetSrvException {
+        OutputStream os = ctx.getOutputStream();
+        try {
+            if (result.length == EMPTY_SIZE) {
+                os.write(Utils.longToBytesBE(ERROR_CODE));
+                throw new PS3NetSrvException("Empty byte array to send to response");
+            }
+            os.write(result);
+        } finally {
+            os.flush();
+        }
     }
 
     @Override
